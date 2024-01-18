98bb3c226f41949a8ac74de1dee6319765247fc5
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2024-01-18 10:09:08 +0545

    features/passwordhash

--- controller/Admin_Controller.ts
+++ controller/Admin_Controller.ts
@@ -19,15 +19,26 @@ export const CreateVendor = async (
     phone,
   } = <CreateVendorInput>req.body;
 
-  const createvandor = await Vandor.create({
+  // checking vendor exist or not
+
+  const existingVendor = await Vandor.findOne({ email: email });
+
+  if (existingVendor !== null) {
+    return res.json({
+      message: "vendor already exist with the given email id",
+    });
+  }
+
+  // if doesnt exist it will create a new vendor
+  const createdvandor = await Vandor.create({
     name: name,
     address: address,
-
     pincode: pincode,
     foodType: foodType,
     email: email,
     password: password,
     ownerName: ownerName,
+    salt: "sagarererer  ssahshhash",
     phone: phone,
     rating: 0,
     serviceAvailable: false,
@@ -35,14 +46,7 @@ export const CreateVendor = async (
   });
 
   res.json({
-    name,
-    address,
-    pincode,
-    foodType,
-    email,
-    password,
-    ownerName,
-    phone,
+    createdvandor,
   });
 };
 
--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ import express from "express";
 
 import bodyParser from "body-parser";
 
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
+import { ConnectOptions, connect } from "mongoose";
 
 import {
   Admin_routes,
@@ -12,10 +13,6 @@ import {
 
 import { MONGO_URI } from "./config";
 
-
-
-  
-
 const app = express();
 
 // bodyparser for json type
@@ -23,6 +20,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 8832;
+
+const connectionOptions: ConnectOptions = {};
+
+// this will execute a connection with  db and server
+async function run() {
+  await connect(MONGO_URI, connectionOptions);
+  app.listen(PORT, () => {
+    console.clear();
+    console.log(`server is starting at port ${PORT}`);
+  });
+}
 
 app.use("/admin", Admin_routes);
 app.use("/vendor", Vendor_routes);
@@ -32,7 +40,5 @@ app.get("/", (req, res) => {
   return res.json("Hello from the backend");
 });
 
-app.listen(PORT, () => {
-  console.clear();
-  console.log(`server is starting at port ${PORT}`);
-});
+// calling run function
+run();
